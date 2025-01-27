1b1c9f023d12d55ccd18eb10dd85ea844bf1267b
Raza Abbas <157038022+raza-abbas-23sw@users.noreply.github.com>
2025-01-27 17:56:21 +0500

    Update script.js

--- script.js
+++ script.js
@@ -8,3 +8,42 @@ navLinks.forEach(link => {
     link.classList.add('activ');
   });
 });
+
+gsap.from(".col-md-4",{
+  y:20,
+  opacity:0,
+  duration:1,
+  // stagger:1
+})
+gsap.from(".hult-section img",{
+  y:70,
+  opacity:0,
+  duration:1,
+  // delay:1
+  // stagger:1
+})
+
+gsap.from(".hult-content",{
+  y:-70,
+  opacity:0,
+  duration:1,
+  // delay:1
+  // stagger:1
+})
+gsap.from("#home h2, #home h1",{
+  scale:0,
+  delay:1,
+  stagger:.5
+  // opacity:0,
+  // duration:1,
+  // delay:1
+  // stagger:1
+})
+gsap.from(".achievement-box",{
+  y:10,
+  scale:0,
+  opacity:0,
+  duration:.3,
+  // delay:1
+  stagger:1
+})
